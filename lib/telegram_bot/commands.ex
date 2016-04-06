0dfad5bf62110f4ae2dbdb9ea31c203820622d66
rekyuu <senkan.re.kyuu@gmail.com>
2016-04-06 13:21:29 -0600

    initial commit

--- lib/telegram_bot/commands.ex
+++ lib/telegram_bot/commands.ex
@@ -1,17 +1,9 @@
 defmodule TelegramBot.Commands do
   use TelegramBot.Module
 
-  command "ping", do: reply "Pong!"
+  command "start", do: reply "Okay!"
 
-  command "say" do
-    [_ | repeat] = String.split(msg.text)
-    reply repeat |> Enum.join(" ")
-  end
-
-  match ["hello", "hi", "hey", "sup"] do
-    case msg do
-      %{from: %{username: name}}   -> reply "Hi there, @#{name}!"
-      %{from: %{first_name: name}} -> reply "Hi there, #{name}!"
-    end
+  match _ do
+    nil
   end
 end
