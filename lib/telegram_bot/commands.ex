3e13b135cde944d2601bebda8f83c88d03bd10e5
rekyuu <senkan.re.kyuu@gmail.com>
2016-04-06 19:56:09 -0600

    updated v1.0.1

--- lib/telegram_bot/commands.ex
+++ lib/telegram_bot/commands.ex
@@ -5,6 +5,7 @@ defmodule TelegramBot.Commands do
   command "help" do
     reply_no_preview "I'm light cruiser Kitakami, nice ta meet 'ya.\n"
     <> "I'll notify you if someone says a specific word.\n"
+    <> "Be sure to message me privately or you won't get any messages from me!\n"
     <> "\n"
     <> "/help - Lists commands.\n"
     <> "/start - Adds you for list creation.\n"
@@ -17,7 +18,7 @@ defmodule TelegramBot.Commands do
     <> "/unmute - Unmute all notifications.\n"
     <> "/stop - Removes you completely and stops notifications."
     <> "\n"
-    <> "Source (v1.0.0): https://github.com/rekyuu/elixir_telegram_bot/tree/kitakami"
+    <> "Source (v1.0.1): https://github.com/rekyuu/elixir_telegram_bot/tree/kitakami"
   end
 
   command "start", do: reply create_user(msg.from.username, msg.from.id)
